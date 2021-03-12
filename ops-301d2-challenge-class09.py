800d4f4cad9ea9ea7fb46afc93ba66464b15bb5e
Emilio Muniz <emiliomunizphoto@gmail.com>
2021-03-11 21:30:27 -0800

    Create ops-301d2-challenge-class09.py

---
+++ ops-301d2-challenge-class09.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Script:                   ops-301d2-challenge-class09.py                       
+# Author:                   Emilio Muniz                       
+# Date of latest revision:  3/11/2021    
+# Purpose:                  How to create and print a list.
+
+# Declaration of variables:
+fruits = ["apple", "banana", "cherry", "berry", "orange", "grape", "kiwi", "jack", "passion", "dragon"]
+
+# Declaration of functions:
+
+# Main
+
+# This prints index 4:
+print(fruits[3])
+
+# This prints index 6 to 10:
+print(fruits[5:11])
+
+# This changes the value of index 7:
+fruits[6] = "onion"
+
+print(fruits)
+
+# End
+# References: https://datatofish.com/modify-list-python/ https://github.com/codefellows/seattle-ops-301d2/blob/main/class-09/challenges/DEMO.md
